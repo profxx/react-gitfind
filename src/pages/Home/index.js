974382b1da7ba99fab2082e9213a72775641986b
Alexandre Pimentel <acpimentel@gmail.com>
2024-06-03 00:43:32 -0300

    API completa funcionando, apenas na primeira página dos 30 repositórios do usuário

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,85 @@
+import { useState } from "react";
 import { Header } from "../../components/Header";
+import background from "../../assets/gitlogo.png";
+import "./styles.css";
+import ItemList from "../../components/ItemList";
 
 const App = () => {
+
+  const [user, setUser] = useState('');
+  const [currentUser, setCurrentUser] = useState(null);
+  const [repos, setRepos] = useState(null);
+
+  const handleGetData = async () => {
+      const userData = await fetch(`https://api.github.com/users/${user}`);
+      const newUser = await userData.json();
+
+      if (newUser.name){
+        const {avatar_url, name, bio, login} = newUser;
+        setCurrentUser({avatar_url, name, bio, login});
+
+        const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
+        const newRepos = await reposData.json();
+
+        if (newRepos.length){
+          setRepos(newRepos);
+        }
+
+      }
+  }
+
   return (
     <div className="App">
         <Header />
+        <div className="conteudo">
+          
+          <img src={background} className="background" alt="Imagem de fundo"/>
+          
+          <div className="info">
+
+            <div>
+              <input 
+                  name="usuario" 
+                  placeholder="@username"
+                  value={user}
+                  onChange={event => setUser(event.target.value)} 
+              />
+              <button onClick={handleGetData}>Buscar</button>
+            </div>
+
+            {currentUser?.name ?(
+            <div>
+                  <div className="perfil">
+                      <img 
+                        src={currentUser.avatar_url} 
+                        alt="Imagem github" 
+                        className="profile" 
+                      />
+
+                      <div>
+                          <h3>{currentUser.name}</h3>
+                          <span>{`@${currentUser.login}`}</span>
+                          <p>{currentUser.bio}</p>
+                      </div>
+
+                  </div>
+                
+                  <hr />
+                  {repos?.length ?(
+                  <div>
+                      <h4 className="repositorio">{`Repositórios - Total: ${repos.length}`}</h4>
+                      {repos.map((repo) => (
+                          <ItemList title={repo.name} description={repo.description} />
+                      ))};
+                      
+  
+                  </div>
+                  ):null}
+              </div>
+            ):null}
+
+          </div>
+        </div>
     </div>
   );
 }
